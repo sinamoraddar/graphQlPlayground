00b190cb932d810729e0effefd9aeeb1881bc201
sinamoraddar <sinamoraddar@gmail.com>
2020-08-10 23:14:47 +0430

    add base of sign up  mutation

---
+++ myPlayground/authGraphQLStarte/server/schema/mutation.js
@@ -0,0 +1,18 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
+const UserType = require("./types/user_type");
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    signUp: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+      },
+      resolve(parentValue, args, request) {},
+    },
+  },
+});
+
+module.exports = mutation;
