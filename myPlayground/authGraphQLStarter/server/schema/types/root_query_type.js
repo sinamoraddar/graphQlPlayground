e1607a1e4e692fb0f1a7a7b8e04658102302f47a
sinamoraddar <sinamoraddar@gmail.com>
2020-08-13 00:15:43 +0430

    add user query

--- myPlayground/authGraphQLStarter/server/schema/types/root_query_type.js
+++ myPlayground/authGraphQLStarter/server/schema/types/root_query_type.js
@@ -7,6 +7,9 @@ const RootQueryType = new GraphQLObjectType({
   fields: {
     user: {
       type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      },
     },
   },
 });
