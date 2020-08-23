354317e6904b9e8b5f59eb5c4e5b39dfbb38d865
sinamoraddar <sinamoraddar@gmail.com>
2020-08-23 18:38:26 +0430

    add logout mutation

--- myPlayground/authGraphQLStarter/client/components/header/Header.js
+++ myPlayground/authGraphQLStarter/client/components/header/Header.js
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
+//queries
 import CurrentUser from "../../queries/CurrentUser";
+//mutations
+import LogoutMutation from "../mutations/Logout";
 
 class Header extends Component {
+  handleLogout = (e) => {
+    this.props.mutate({ refetchQueries: [{ query: CurrentUser }] });
+  };
   renderButtons() {
     const { user, loading } = this.props.data;
     if (loading) {
       return;
     }
-    return user ? <div>Logout</div> : <div>You're not signed in</div>;
+    return user ? (
+      <li>
+        <a onClick={this.handleLogout}>Log out</a>
+      </li>
+    ) : (
+      <>
+        <li>
+          <Link to="/signup">Sign up</Link>
+        </li>
+        <li>
+          <Link to="/login">Log in</Link>
+        </li>
+      </>
+    );
   }
   render() {
     return (
       <nav>
-        <div className="nav-wrapper">{this.renderButtons.bind(this)}</div>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons.bind(this)}</ul>
+        </div>
       </nav>
     );
   }
 }
 
-export default graphql(CurrentUser)(Header);
+export default graphql(LogoutMutation)(graphql(CurrentUser)(Header));
